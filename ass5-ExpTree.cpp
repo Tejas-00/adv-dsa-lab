6edde07dc1f69eb0ea8baca45315092fe7cee183
Tejas-00 <tejassonawane1709@gmail.com>
2024-04-06 16:26:25 +0530

    file added

---
+++ ass5-ExpTree.cpp
@@ -0,0 +1,4 @@
+// Expression tree: Write a menu driven C++ program to construct an expression tree from the given prefix expression eg. +--a*bc/def and perform following operations: 1. Traverse it using post order traversal (non recursive) 2. Delete the entire tree 3. Change a tree so that the roles of the left and right pointers are swapped at every node
+
+#include<iostream>
+using namespace std;
